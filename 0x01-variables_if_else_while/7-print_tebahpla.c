72157f4296b04323f0674181accf2a05c99d16c6
Tamuka Lot <mnangagwatamuka@gmail.com>
2022-09-09 14:33:55 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints reversed alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 82; i > 56; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
