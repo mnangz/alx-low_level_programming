b8f115a76b0843870c0bc02b59a427d50da0cfb4
Tamuka Lot <mnangagwatamuka@gmail.com>
2022-09-22 12:17:40 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,9 +2,9 @@
 
 /**
  * cap_string - capitalizes all words of a string
- *  * @s: input string.
- *   * Return: the pointer to dest.
- *    */
+ * @s: input string.
+ * Return: the pointer to dest.
+ */
 
 char *cap_string(char *s)
 {
