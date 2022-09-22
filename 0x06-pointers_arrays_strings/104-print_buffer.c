2adbb2fe413763b4b91a480e1a804c1d896841a3
Tamuka Lot <mnangagwatamuka@gmail.com>
2022-09-22 14:50:18 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -16,27 +16,30 @@ void print_buffer(char *b, int size)
 		printf("\n");
 	else
 	{
-		printf("%.8x:", j);
-		for (k = j; k < j + 10; k++)
+		for (j = 0; j < size; j += 10)
 		{
-			if (k % 2 == 0)
-				printf(" ");
-			if (k < size)
-				printf("%.2x", *(b + k));
-			else
-				printf("  ");
-		}
+			printf("%.8x:", j);
+			for (k = j; k < j + 10; k++)
+			{
+				if (k % 2 == 0)
+					printf(" ");
+				if (k < size)
+					printf("%.2x", *(b + k));
+				else
+					printf("  ");
+			}
 
-		printf(" ");
-		for (l = j; l < j + 10; l++)
-		{
-			if (l >= size)
-				break;
-			if (*(b + l) < 32 || *(b + l) > 126)
-				printf("%c", '.');
-			else
-				printf("%c", *(b + l));
+			printf(" ");
+			for (l = j; l < j + 10; l++)
+			{
+				if (l >= size)
+					break;
+				if (*(b + l) < 32 || *(b + l) > 126)
+					printf("%c", '.');
+				else
+					printf("%c", *(b + l));
+			}
+			printf("\n");
 		}
-		printf("\n");
 	}
 }
