470c69de4341a6efca7a3a25e18f1d5d09c2e047
Tamuka Lot <mnangagwatamuka@gmail.com>
2022-10-04 14:41:25 -0700

    4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -4,7 +4,7 @@
 /**
  * free_grid - frees a 2 dimensional grid.
  * @grid: multidimensional array of integers.
- * height: height of the grid.
+ * @height: height of the grid.
  *
  * Return: no return
  */
